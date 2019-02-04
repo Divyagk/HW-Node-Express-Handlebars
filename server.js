0c565b66ecb3a379891d9d2dae3a3b838cd2e2be
divya <gopalakrishnandivya6@gmail.com>
2019-02-04 16:28:06 -0700

    updated

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require("express");
-
 var PORT = process.env.PORT || 8080;
 
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
+// app.use(express.static(__dirname + '/public'));
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
@@ -21,6 +21,7 @@ app.set("view engine", "handlebars");
 var routes = require("./controllers/burgers_controller.js");
 
 app.use("/",routes);
+
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
